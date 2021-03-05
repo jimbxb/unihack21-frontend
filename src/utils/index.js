c6e46e4b869bda7efadc82ef792c02bbd421c410
James Barnes <jamesbarnes2505@gmail.com>
2021-03-06 02:12:36 +1100

    added axios

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,40 +1,36 @@
+import axios from 'axios';
+
 
 export const getModels = () => {
-  return fetch(
-    "/model",
-    { method: "GET" }
-  );
+  return axios({
+    url: "TODO/model",
+    method: "GET"
+  });
 };
 
 export const pushModel = (data) => {
-  return fetch(
-    "/model", 
-    {
-      method: "POST",
-      body: JSON.stringify(data)
-    }
-  );
+  return fetch({
+    url: "TODO/model", 
+    method: "POST",
+    body: JSON.stringify(data)
+  });
 }
 
 export const trainModel = (key, data) => {
-  return fetch(
-    `/train/${key}`,
-    {
-      method: "POST",
-      body: JSON.stringify(data)
-    }
-  );
+  return axios({
+    url: `TODO/train/${key}`,
+    method: "POST",
+    body: JSON.stringify(data)
+  });
 }
 
 export const evalModel = (key, data) => {
-  return fetch(
-    "/eval",
-    {
-      method: "POST",
-      body: JSON.stringify({
-        key,
-        data
-      })
-    }
-  );
+  return axios({
+    url: "TODO/eval",
+    method: "POST",
+    body: JSON.stringify({
+      key,
+      data
+    })
+  });
 }
