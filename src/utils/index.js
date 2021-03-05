e450ccdd7de54cd1408e979f987d1f3a3e842224
James Barnes <jamesbarnes2505@gmail.com>
2021-03-05 21:12:42 +1100

    basic api funcs

---
+++ src/utils/index.js
@@ -0,0 +1,39 @@
+
+export const getModels = () => {
+  return fetch(
+    "/model",
+    { method: "GET" }
+  );
+};
+
+export const callModel = (data) => {
+  return fetch(
+    "/model", 
+    {
+      method: "POST",
+      body: JSON.stringify(data)
+    }
+  );
+}
+
+export const trainModel = (key, data) => {
+  return fetch(
+    `/train/${key}`,
+    {
+      method: "POST",
+      body: JSON.stringify(data)
+    }
+  );
+}
+
+export const evalModel = (key, data) => {
+  return fetch(
+    "/eval",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        key, data
+      })
+    }
+  );
+}
