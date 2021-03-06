bca3b38d4617072a79fe753ba9b27f59c43a623a
James Barnes <jamesbarnes2505@gmail.com>
2021-03-06 23:07:31 +1100

    replaced app

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Status from "./components/status";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Status />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
